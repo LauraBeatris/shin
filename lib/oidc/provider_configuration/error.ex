315d5254622369528c57042cb4fee03cbb552769
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2024-03-23 16:20:07 -0300

    Add errors atoms to typespec

--- lib/oidc/provider_configuration/error.ex
+++ lib/oidc/provider_configuration/error.ex
@@ -1,6 +1,13 @@
 defmodule ShinAuth.OIDC.ProviderConfiguration.Error do
   @type t :: %__MODULE__{
-          tag: atom(),
+          tag:
+            :malformed_discovery_endpoint
+            | :discovery_endpoint_unreachable
+            | :authorization_endpoint_unreachable
+            | :token_endpoint_unreachable
+            | :malformed_jwks_uri_response
+            | :jwks_uri_unreachable
+            | :missing_issuer_attribute,
           message: String.t(),
           data: any() | nil,
           endpoint: String.t() | nil
