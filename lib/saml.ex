f1c45583d9d1fb66288b537a107bc46a4d8615ac
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2024-03-24 13:47:56 -0300

    Validate XML input

--- lib/saml.ex
+++ lib/saml.ex
@@ -5,12 +5,26 @@ defmodule ShinAuth.SAML do
 
   alias ShinAuth.SAML.Request
 
-  def decode_saml_request(saml_request) do
-    parsed_saml_request = DataSchema.to_struct(saml_request, Request)
+  def decode_saml_request(""), do: {:error, "Empty SAML request"}
 
-    case parsed_saml_request do
-      {:error, _} -> {:error, "Invalid SAML request"}
-      _ -> parsed_saml_request
+  def decode_saml_request(saml_request) do
+    if is_valid_xml?(saml_request) do
+      case DataSchema.to_struct(saml_request, Request) do
+        {:ok, parsed_saml_request} -> {:ok, parsed_saml_request}
+        {:error, _} = error -> error
+      end
+    else
+      {:error, "Invalid SAML request"}
+    end
+  end
+
+  defp is_valid_xml?(xml_string) do
+    try do
+      SweetXml.parse(xml_string, quiet: true)
+      true
+    catch
+      :exit, _ -> false
+      :error, _ -> false
     end
   end
 end
